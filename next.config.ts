6fa4d60f4d896f5cc95a587b07ad9a53533b1fe9
Daris Chen <daris.chen@gmail.com>
2025-02-20 01:04:05 -0800

    Next.js Project Initialized

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+<<<<<<< HEAD
+=======
+  reactStrictMode: true,
+>>>>>>> 46be486 (Initial commit from Create Next App)
 };
 
 export default nextConfig;
